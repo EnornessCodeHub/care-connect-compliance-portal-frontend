5e91ea4f01a7a0a841f013ab5f6b9d14d961208e
Hamza <hamza.qureshi@enorness.com>
2025-10-27 18:42:57 +0500

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true,
     port: 7001,
+    allowedHosts: ['*'], // allow all hosts
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
